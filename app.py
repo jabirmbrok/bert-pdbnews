0b138f66841a393cdf976548bc84f0bb1a4e6ee1
mohjabirmubarok <jabirmubarok@gmail.com>
2025-05-18 17:08:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.title("Dashboard Klasifikasi Berita Pergerakan GDP Indonesia")
 
+# Load data
 data = pd.read_csv("dataset.csv")
 data['pdb_label'] = data['pdb_label'].map({1: 'Naik', -1: 'Turun'}).fillna('Tidak diketahui')
 
 sector_label = st.selectbox("Pilih Sektor Industri:", options=data['sector_label'].dropna().unique())
 filtered_data = data[data['sector_label'] == sector_label].copy()
 
-def label_with_color(x):
-    if x == 'Naik':
-        return "🟢 Naik"
-    elif x == 'Turun':
-        return "🔴 Turun"
-    else:
-        return x
-
-filtered_data['pdb_label_color'] = filtered_data['pdb_label'].apply(label_with_color)
-
-cols_to_show = ['title', 'publish_date', 'sector_label', 'pdb_label_color']
+cols_to_show = ['title', 'publish_date', 'sector_label', 'pdb_label']
 data1 = filtered_data[cols_to_show].copy()
-data1.reset_index(drop=True, inplace=True)
 
-gb = GridOptionsBuilder.from_dataframe(data1)
-gb.configure_default_column(editable=False, groupable=False)
-gb.configure_column("title", width=300, header_name="Judul Berita")
-gb.configure_column("publish_date", width=150, header_name="Tanggal Terbit")
-gb.configure_column("sector_label", width=150, header_name="Sektor Industri")
-gb.configure_column("pdb_label_color", width=120, header_name="Prediksi")
+# Fungsi untuk highlight warna baris berdasarkan nilai kolom pdb_label
+def highlight_pdb_label(row):
+    color = ''
+    if row['pdb_label'] == 'Naik':
+        color = 'background-color: green; color: white;'
+    elif row['pdb_label'] == 'Turun':
+        color = 'background-color: red; color: white;'
+    else:
+        color = ''
+    return [color] * len(row)
 
-grid_options = gb.build()
+styled_df = data1.style.apply(highlight_pdb_label, axis=1)
 
-AgGrid(data1, gridOptions=grid_options, height=400)
+st.dataframe(styled_df, height=400)
 
 st.subheader("Hasil Klasifikasi")
 col1, col2, col3, col4 = st.columns(4)
